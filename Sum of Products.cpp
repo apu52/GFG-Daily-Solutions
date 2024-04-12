2225cc858799eb3410a5036efd31169f5a991d3d
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-04-12 22:28:26 +0530

    Create Sum of Products.cpp

---
+++ Sum of Products.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    long long pairAndSum(int n, long long arr[]) {
+        long long ans = 0;
+
+        for(int i = 0;i < 32; i++) {
+            long long count = 0;
+            for(int j = 0;j < n; j++) {
+                if(arr[j] & (1<<i))
+                    count++;
+            }
+
+            ans += 1ll*(count*(count-1)/2)*(1ll<<i);
+        }
+
+        return ans;
+    }
+};
