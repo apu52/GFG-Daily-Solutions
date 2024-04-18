a1f43b6f33e37046f59263ba8e4d29ec7e451ecf
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-04-18 19:42:02 +0530

    Create Two Repeated Elements.cpp

---
+++ Two Repeated Elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+  vector<int> twoRepeated (int arr[], int n) {
+        vector<int> ans;
+        for(int i=0; i<n+2; i++){
+            int ind = abs(arr[i]);
+            if(arr[ind]>0){
+                arr[ind] = -arr[ind];
+            }
+            else{
+                ans.push_back(ind);
+            }
+        }
+        return ans;
+  }
+};
