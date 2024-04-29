8d5433989619887fec8c8ba42de44603a0c19172
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-04-29 09:42:44 +0530

    Create Remove every kth node.cpp

---
+++ Remove every kth node.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    Node* deleteK(Node *head,int K){
+    if(K == 1) return nullptr;
+    Node* s = head;
+    int cnt = 0;
+    for(Node* s = head; s != nullptr; s = s->next) {
+        cnt++;
+        if(cnt == K-1){
+            cnt = 0;
+            if(s->next)
+                s->next = s->next->next;
+        }
+
+    }
+    return head;
+}
+};
