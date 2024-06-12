ef5eab1f40063b3feabf026e38c711e88d361da3
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-06-13 00:34:24 +0530

    Update Padovan_Sequence_13-6-24.cpp

--- Padovan_Sequence_13-6-24.cpp
+++ Padovan_Sequence_13-6-24.cpp
@@ -8,7 +8,7 @@ class Solution
        long long int mod = 1000000007;
        for(int i=0;i<=n;i++){
            if(i<=2) arr[i]=1ll;
-           else arr[i]=((arr[i-2]%mod)+(arr[i-3]%mod))%mod;
+           else arr[i]=(arr[i-2]+arr[i-3])%mod;
        }
        
        return arr[n];
