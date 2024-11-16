11a2993903873c749e0afc483207aca2fd9f58a9
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-11-16 21:30:11 +0530

    Create Move All Zeroes to End.cpp

---
+++ Move All Zeroes to End.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int i=0;
+        for(int j=0; j<arr.size(); j++){
+            if(arr[j] != 0) {
+                swap(arr[i], arr[j]);
+                i++;
+            }
+        }
+    }
+};
