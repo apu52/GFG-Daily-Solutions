cb2d0abf1bd470a4c011545691f1c19d4a4067f9
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-11-27 12:37:49 +0530

    Create Repetitive Addition Of Digits.cpp

---
+++ Repetitive Addition Of Digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int singleDigit(int n) {
+        
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10; 
+                n /= 10;       
+            }
+            n = sum; 
+        }
+        return n;
+    }
+};
