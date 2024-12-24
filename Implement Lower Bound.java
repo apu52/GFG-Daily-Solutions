0d3bde00d2954fd25bd3441717d0c700bcdb91e6
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-12-24 16:08:41 +0530

    Create Implement Lower Bound.java

---
+++ Implement Lower Bound.java
@@ -0,0 +1,14 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
