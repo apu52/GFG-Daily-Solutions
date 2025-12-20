3b046e3c878f58f21f16c49ca1f395af4a48dfe3
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-12-20 11:45:32 +0530

    Add function to find moves for chairs and passengers
    Implement function to calculate moves for passengers.

---
+++ Bus Conductor.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int findMoves(vector<int>& chairs, vector<int>& passengers) {
+        // code here
+        sort(chairs.begin(),chairs.end());
+        sort(passengers.begin(),passengers.end());
+        int moves=0;
+        int n=chairs.size();
+        for(int i=0;i<n;i++){
+            moves+=abs(chairs[i]-passengers[i]);
+        }
+        return moves;
+    }
+};
