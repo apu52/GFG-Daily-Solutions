4dc09f809367a19235f7fd6bea525cf72ce17af0
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-12-22 21:22:21 +0530

    Add rowWithMax1s function to Solution class
    Implement a method to find the row with the maximum number of 1s in a 2D array.

---
+++ Row with max 1s new.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        unordered_map<int,int>ans;
+        int maxi=-1;
+        int ind=-1;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[i].size();j++){
+                if(arr[i][j]==1){
+                    ans[i]++;
+                    if(maxi<ans[i]){
+                        maxi=ans[i];
+                        ind=i;
+                    }
+                }
+            }
+        }
+        return ind;
+        
+    }
+};
