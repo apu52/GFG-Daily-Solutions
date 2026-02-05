e5a86b8d03beb5ef5b3335267020fef8508bbce9
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2026-02-05 20:31:48 +0530

    Create Last Moment Before All Ants Fall Out.cpp

---
+++ Last Moment Before All Ants Fall Out.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int maxi=INT_MIN;
+        for(int i=0;i<left.size();i++)
+        {
+            if(left[i]>maxi)
+            {
+                maxi=left[i];
+            }
+        }
+        for(int i=0;i<right.size();i++)
+        {
+            maxi=max(maxi,n-right[i]);
+        }
+        return maxi;
+    }
+};
