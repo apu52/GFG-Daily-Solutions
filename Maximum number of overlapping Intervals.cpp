742659992efc70390b1589a04137e034a95c6275
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2026-02-17 20:20:44 +0530

    Create Maximum number of overlapping Intervals.cpp

---
+++ Maximum number of overlapping Intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int overlapInt(vector<vector<int>>& arr) {
+        int n = arr.size();
+        
+        vector<int> start(n), end(n);
+        
+        for(int i = 0; i < n; i++) {
+            start[i] = arr[i][0];
+            end[i] = arr[i][1];
+        }
+        
+        sort(start.begin(), start.end());
+        sort(end.begin(), end.end());
+        
+        int i = 0, j = 0;
+        int current = 0, maximum = 0;
+        
+        while(i < n && j < n) {
+            if(start[i] <= end[j]) {
+                current++;
+                maximum = max(maximum, current);
+                i++;
+            } else {
+                current--;
+                j++;
+            }
+        }
+        
+        return maximum;
+    }
+};
