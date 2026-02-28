d3f55dc8656627edaefad62a217a34041f358cf3
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2026-02-28 23:49:23 +0530

    Create Find the closest pair from two arrays.cpp

---
+++ Find the closest pair from two arrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    vector<int> findClosestPair(vector<int> &arr, vector<int> &brr, int x) {
+        vector<int> values(2,0);
+        int n=arr.size(), m=brr.size();
+        int i=0;
+        int j=m-1;
+        int ans=INT_MAX;
+        int sum=arr[0]+brr[m-1];
+        while(i<n && j>=0)
+        {
+            sum=arr[i]+brr[j];
+            if(ans>abs(sum-x))
+            {
+                ans=abs(sum-x);
+                values[0]=arr[i];
+                values[1]=brr[j];
+            }
+            
+            if(sum<=x)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return values;
+    }
+};
