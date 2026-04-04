a9dadd1cf36b13af3e305f9dbe7efeb7802b6ebb
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2026-04-04 17:07:43 +0530

    Create Gray Code.cpp

---
+++ Gray Code.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<string> graycode(int n) {
+      vector<string> prev={"0", "1"};
+      if(n==1) return prev;
+      
+      for(int i=2; i<=n; i++){
+        vector<string> next;
+        int pn=prev.size();
+        
+        for(int j=0; j<pn; j++){
+          next.push_back("0" + prev[j]);    
+        }        
+        for(int j=pn-1; j>=0; j--){
+          next.push_back("1" + prev[j]);    
+        }
+        
+        prev = next;
+              
+      }
+      
+      return prev;
+    }
+};
