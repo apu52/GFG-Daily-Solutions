871fb3be94ba095c146b6009f77adba8993409bb
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2026-05-09 08:53:08 +0530

    Create Count Spanning Trees in a Graph.cpp

---
+++ Count Spanning Trees in a Graph.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+
+    long long determinant(vector<vector<double>> mat, int n) {
+
+        double det = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            int pivot = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (abs(mat[j][i]) > abs(mat[pivot][i]))
+                    pivot = j;
+            }
+
+            if (abs(mat[pivot][i]) < 1e-9)
+                return 0;
+
+            if (pivot != i) {
+                swap(mat[pivot], mat[i]);
+                det *= -1;
+            }
+
+            det *= mat[i][i];
+
+            for (int j = i + 1; j < n; j++) {
+
+                double factor = mat[j][i] / mat[i][i];
+
+                for (int k = i; k < n; k++) {
+                    mat[j][k] -= factor * mat[i][k];
+                }
+            }
+        }
+
+        return round(det);
+    }
+
+    int countSpanTree(int n, vector<vector<int>>& edges) {
+
+        vector<vector<double>> lap(n, vector<double>(n, 0));
+
+        // Build Laplacian Matrix
+        for (auto &e : edges) {
+
+            int u = e[0];
+            int v = e[1];
+
+            lap[u][u]++;
+            lap[v][v]++;
+
+            lap[u][v]--;
+            lap[v][u]--;
+        }
+
+        // Remove last row & column
+        vector<vector<double>> cofactor(n - 1,
+                                        vector<double>(n - 1));
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                cofactor[i][j] = lap[i][j];
+            }
+        }
+
+        return determinant(cofactor, n - 1);
+    }
+};
